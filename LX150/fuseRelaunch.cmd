65cee832d8c03538303a5882b139d75a166df58a
Deyan <didolevski@gmail.com>
2016-11-11 16:09:15 +0000

    Regenerated bitstream

--- LX150/fuseRelaunch.cmd
+++ LX150/fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -lib "secureip" -o "/media/data/git/jcfpga/LX150/ADC_CTRL_isim_beh.exe" -prj "/media/data/git/jcfpga/LX150/ADC_CTRL_beh.prj" "work.ADC_CTRL" 
+-intstyle "ise" -incremental -lib "secureip" -o "/media/data/git/jcfpga/LX150/ADC_CTRL_isim_beh.exe" -prj "/media/data/git/jcfpga/LX150/ADC_CTRL_beh.prj" "ADC_CTRL" 
--- LX150/isim/ADC_CTRL_isim_beh.exe.sim/work/a_1408110992_0912031422.c
+++ LX150/isim/ADC_CTRL_isim_beh.exe.sim/work/a_1408110992_0912031422.c
@@ -36,12 +36,12 @@ static void work_a_1408110992_0912031422_p_0(char *t0)
     char *t7;
     char *t8;
 
-LAB0:    xsi_set_current_line(158, ng0);
+LAB0:    xsi_set_current_line(108, ng0);
 
-LAB3:    t1 = (t0 + 1672U);
+LAB3:    t1 = (t0 + 1032U);
     t2 = *((char **)t1);
     t3 = *((unsigned char *)t2);
-    t1 = (t0 + 5248);
+    t1 = (t0 + 5512);
     t4 = (t1 + 56U);
     t5 = *((char **)t4);
     t6 = (t5 + 56U);
@@ -49,7 +49,7 @@ LAB3:    t1 = (t0 + 1672U);
     *((unsigned char *)t7) = t3;
     xsi_driver_first_trans_fast(t1);
 
-LAB2:    t8 = (t0 + 5152);
+LAB2:    t8 = (t0 + 5400);
     *((int *)t8) = 1;
 
 LAB1:    return;
@@ -68,12 +68,44 @@ static void work_a_1408110992_0912031422_p_1(char *t0)
     char *t7;
     char *t8;
 
-LAB0:    xsi_set_current_line(167, ng0);
+LAB0:    xsi_set_current_line(159, ng0);
 
 LAB3:    t1 = (t0 + 1672U);
     t2 = *((char **)t1);
     t3 = *((unsigned char *)t2);
-    t1 = (t0 + 5312);
+    t1 = (t0 + 5576);
+    t4 = (t1 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    *((unsigned char *)t7) = t3;
+    xsi_driver_first_trans_fast(t1);
+
+LAB2:    t8 = (t0 + 5416);
+    *((int *)t8) = 1;
+
+LAB1:    return;
+LAB4:    goto LAB2;
+
+}
+
+static void work_a_1408110992_0912031422_p_2(char *t0)
+{
+    char *t1;
+    char *t2;
+    unsigned char t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+
+LAB0:    xsi_set_current_line(168, ng0);
+
+LAB3:    t1 = (t0 + 1672U);
+    t2 = *((char **)t1);
+    t3 = *((unsigned char *)t2);
+    t1 = (t0 + 5640);
     t4 = (t1 + 56U);
     t5 = *((char **)t4);
     t6 = (t5 + 56U);
@@ -81,7 +113,7 @@ LAB3:    t1 = (t0 + 1672U);
     *((unsigned char *)t7) = t3;
     xsi_driver_first_trans_fast_port(t1);
 
-LAB2:    t8 = (t0 + 5168);
+LAB2:    t8 = (t0 + 5432);
     *((int *)t8) = 1;
 
 LAB1:    return;
@@ -92,7 +124,7 @@ LAB4:    goto LAB2;
 
 extern void work_a_1408110992_0912031422_init()
 {
-	static char *pe[] = {(void *)work_a_1408110992_0912031422_p_0,(void *)work_a_1408110992_0912031422_p_1};
+	static char *pe[] = {(void *)work_a_1408110992_0912031422_p_0,(void *)work_a_1408110992_0912031422_p_1,(void *)work_a_1408110992_0912031422_p_2};
 	xsi_register_didat("work_a_1408110992_0912031422", "isim/ADC_CTRL_isim_beh.exe.sim/work/a_1408110992_0912031422.didat");
 	xsi_register_executes(pe);
 }
