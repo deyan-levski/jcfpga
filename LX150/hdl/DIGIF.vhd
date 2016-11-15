8095f6b4ee7a137d2da55fde914601bcefdc0c77
Deyan <didolevski@gmail.com>
2016-11-15 18:49:03 +0000

    Updated bitstream

--- LX150/hdl/DIGIF.vhd
+++ LX150/hdl/DIGIF.vhd
@@ -16,8 +16,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library UNISIM;
+use UNISIM.VComponents.all;
 
 entity DIGIF is
 	Port ( d_digif_sck : in  STD_LOGIC;
