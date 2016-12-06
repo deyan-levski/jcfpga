2b898688646af55d01987bea236cfc6ba4801570
Deyan <didolevski@gmail.com>
2016-12-06 10:53:54 +0000

    Updated bitstream

--- LX150/hdl/SREG_CONTROL.vhd
+++ LX150/hdl/SREG_CONTROL.vhd
@@ -187,27 +187,27 @@ begin
 			MEM_FLAG <= '0';
 			INC_MEM_ADD <= '0';
 	
---		elsif RX_ACK = '1' and RX_ACK_OLD = '0' then
-		elsif (RX_ACK'event and RX_ACK = '1') and MEM_FLAG = '0' then
+		elsif RX_ACK = '1' and RX_ACK_OLD = '0' then
+--		elsif (RX_ACK'event and RX_ACK = '1') then
 --			if MEM_FLAG = '0' then
-			SPI_DATA_BUFFER(127 downto 0) <= SPI_DATA_BUFFER(135 downto 8);
-			SPI_DATA_BUFFER(135 downto 128) <= RX_WORD(7 downto 0);
+--			SPI_DATA_BUFFER(127 downto 0) <= SPI_DATA_BUFFER(135 downto 8);
+--			SPI_DATA_BUFFER(135 downto 128) <= RX_WORD(7 downto 0);
 --			else
---			MEM_DATA_BUFFER(23 downto 0) <= MEM_DATA_BUFFER(31 downto 8);
---			MEM_DATA_BUFFER(31 downto 24) <= RX_WORD(7 downto 0);
+			if MEM_FLAG = '1' then
+			MEM_DATA_BUFFER(23 downto 0) <= MEM_DATA_BUFFER(31 downto 8);
+			MEM_DATA_BUFFER(31 downto 24) <= RX_WORD(7 downto 0);
 --			end if;
 			WORD_COUNTER := WORD_COUNTER + 1;
 			INC_MEM_ADD <= '0';
+			end if;
 
-		end if;
-
-		if (RX_ACK = '1' and RX_ACK_OLD = '0') and MEM_FLAG = '1' then
-
-			MEM_DATA_BUFFER(23 downto 0) <= MEM_DATA_BUFFER(31 downto 8);
-			MEM_DATA_BUFFER(31 downto 24) <= RX_WORD(7 downto 0);
+		elsif (RX_ACK'event and RX_ACK = '1') then
+			if MEM_FLAG = '0' then
+			SPI_DATA_BUFFER(127 downto 0) <= SPI_DATA_BUFFER(135 downto 8);
+			SPI_DATA_BUFFER(135 downto 128) <= RX_WORD(7 downto 0);
 			WORD_COUNTER := WORD_COUNTER + 1;
 			INC_MEM_ADD <= '0';
-
+			end if;
 		end if;
 
 		RX_ACK_OLD <= RX_ACK;
