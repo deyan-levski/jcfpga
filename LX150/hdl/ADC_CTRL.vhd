f1332438bb5c1ef0e0f54f400e7d992e5906ae7b
Deyan <didolevski@gmail.com>
2016-12-09 13:38:13 +0000

    Updated bitstream

--- LX150/hdl/ADC_CTRL.vhd
+++ LX150/hdl/ADC_CTRL.vhd
@@ -727,7 +727,7 @@ begin
 		elsif (rising_edge(CLOCK_50)) then
 
 			if (LVAL_SEQ = '1' and LVAL_SEQ_OLD = '0') or (stflag = 1) then
-				if digif_rst_cnt = ((134*sdrat)+2) then	-- 134-6=128 words + offset 2
+				if digif_rst_cnt = ((135*sdrat)+2) then	-- 134-6=128 words + offset 2
 					digif_rst_cnt := 0;
 					d_digif_serial_rst <= '1';
 					stflag := 0;
